f01e4a8ff8a71749516187645e7aba5be031930f
MLutpai <133844603+MLutpai@users.noreply.github.com>
2023-05-17 14:43:55 +0700

    book_stack.py

--- book_stack.py
+++ book_stack.py
@@ -1,27 +1,47 @@
-def tambah_buku(nama ,penulis, stack):
-    stack.append((nama, penulis))
+stack = [] 
 
-def print_top_book(stack):
-    if stack:
-        print(stack[-1])
-    else:
-        print("Stack kosong.")
-        
-stack = []
+def tambah_buku(stack, buku_baru): 
+    stack.append(buku_baru) 
+    print(f" {buku_baru} berhasil ditambahkan ke dalam stack.") 
 
-while True:
-    buku = input("Masukan nama buku: ")
-    if not buku:
-        break
-    penulis = input("Masukan nama penulis: ")
-    if not penulis:
-        break
-    if penulis not in stack:
-        print_top_book(stack)
-        tambah_buku(buku, penulis, stack)
-        print_top_book(stack)
-
-if stack:
-    print_top_book(stack)
-else:
-    print("Stack Kosong.")
+def tambah_pengarang(stack, pengarang_baru):
+    stack.append(pengarang_baru) 
+    print(f" {pengarang_baru} berhasil ditambahkan ke dalam stack.") 
+    
+def hapus_buku_terakhir(stack): 
+    if len(stack) == 0: 
+        print("Stack kosong, tidak ada buku yang dapat dihapus.") 
+    else: 
+        buku_terakhir = stack.pop() 
+        print(f" {buku_terakhir} berhasil dihapus dari stack.") 
+    
+def tampilkan_buku_teratas(stack): 
+    if len(stack)==0: 
+        print("Stack kosong, tidak ada barang yang dapat ditampilkan.") 
+    else: 
+        buku_teratas = stack[-1] 
+        print(f"Barang teratas di dalam stack adalah {buku_teratas}.") 
+    
+while True: 
+    print("\nGudang saat ini:", stack) 
+    print("Menu:") 
+    print("1. Tambah Buku") 
+    print("2. Hapus Buku Terakhir") 
+    print("3. Tampilkan Barang Teratas") 
+    print("4. Keluar") 
+    pilihan = input("Masukkan pilihan Anda (1/2/3/4): ") 
+    
+    if pilihan == "1": 
+        buku_baru = input("Masukkan nama buku yang akan ditambahkan: ") 
+        tambah_buku (stack, buku_baru)
+        pengarang_baru = input("Masukkan nama pengarang yang akan ditambahkan: ") 
+        tambah_pengarang (stack, pengarang_baru) 
+    elif pilihan == "2": 
+        hapus_buku_terakhir(stack) 
+    elif pilihan == "3": 
+        tampilkan_buku_teratas(stack) 
+    elif pilihan == "4": 
+        print("Terima kasih telah menggunakan program ini.") 
+        break 
+    else: 
+        print("Pilihan tidak valid. Silakan masukkan pilihan yang benar.") 
